97cd47fa5bbbaec8e261f57c3cb2c8d07d41e0ed
Michał Borzęcki <michal.borzecki@hotmail.com>
2019-10-21 15:10:01 +0200

    VFS-5813 new backend errors translations

--- addon/utils/get-error-description.js
+++ addon/utils/get-error-description.js
@@ -2,13 +2,15 @@
  * Unpack string with error from backend rejected request
  *
  * @module utils/get-error-description
- * @author Michal Borzecki
- * @copyright (C) 2018 ACK CYFRONET AGH
+ * @author Michał Borzęcki
+ * @copyright (C) 2019 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
 import { htmlSafe, isHTMLSafe } from '@ember/string';
+import { getProperties } from '@ember/object';
 import Ember from 'ember';
+import _ from 'lodash';
 
 const i18nPrefix = 'errors.backendErrors.';
 
@@ -22,40 +24,127 @@ const i18nPrefix = 'errors.backendErrors.';
  * @return {object}
  */
 export default function getErrorDescription(error, i18n) {
-  const errorId = error.id;
   let message;
-  let errorJson;
+  let stringifyError = false;
 
   if (typeof error === 'object' && error.id) {
-    message = i18n.t(i18nPrefix + errorId, error.details);
-    if (message.toString().startsWith('<missing-')) {
-      message = undefined;
-    }
-    try {
-      errorJson = JSON.stringify(error);
-    } catch (e) {
-      errorJson = undefined;
-    }
+    message = findTranslationForError(i18n, error);
+    stringifyError = true;
   } else if (typeof error === 'object' && error.message) {
     message = error.message;
   } else if (isHTMLSafe(error)) {
     message = error;
   } else {
-    try {
-      errorJson = JSON.stringify(error);
-    } catch (e) {
-      if (!(e instanceof TypeError)) {
-        throw error;
-      }
+    stringifyError = true;
+  }
+
+  return {
+    message: toPrintableString(message),
+    errorJsonString: stringifyError ? toPrintableJson(error) : undefined,
+  };
+}
+
+function findTranslationForError(i18n, error) {
+  const {
+    id: errorId,
+    details: errorDetails,
+  } = getProperties(error, 'id', 'details');
+
+  let errorIdToTranslate = errorId;
+  let errorDetailsToTranslate = errorDetails;
+
+  // Errors with non-standard translation method
+  switch(errorId) {
+    case 'posix': {
+      const errnoTranslation =
+        findTranslation(i18n, `${i18nPrefix}translationParts.posixErrno.${errorDetails.errno}`);
+      errorDetailsToTranslate = _.assign({}, errorDetails, {
+        errno: errnoTranslation,
+      });
+      break;
+    }
+    case 'badAudienceToken':
+    case 'badValueToken': {
+      const tokenError = errorDetails.tokenError || {};
+      const tokenErrorTranslation =
+        findTranslation(i18n, i18nPrefix + tokenError.id, tokenError.details);
+      errorDetailsToTranslate = _.assign({}, errorDetails, {
+        tokenError: tokenErrorTranslation,
+      });
+      break;
+    }
+    case 'notAnAccessToken': {
+      const receivedTranslation =
+        findTokenTypeTranslation(i18n, errorDetails.received);
+      errorDetailsToTranslate = _.assign({}, errorDetails, {
+        received: receivedTranslation,
+      });
+      break;
+    }
+    case 'notAnInviteToken': {
+      const expectedTranslation =
+        findTokenTypeTranslation(i18n, errorDetails.expected);
+      const receivedTranslation =
+        findTokenTypeTranslation(i18n, errorDetails.received);
+      errorDetailsToTranslate = _.assign({}, errorDetails, {
+        expected: expectedTranslation,
+        received: receivedTranslation,
+      });
+      break;
+    }
+    case 'tokenAudienceForbidden': {
+      const audience = errorDetails.audience || {};
+      const audienceTranslation = `${audience.type}:${audience.id}`;
+      errorDetailsToTranslate = _.assign({}, errorDetails, {
+        audience: audienceTranslation,
+      });
+      break;
     }
   }
-  message = message ?
-    htmlSafe(Ember.Handlebars.Utils.escapeExpression(message)) : undefined;
-  errorJson = errorJson ?
-    htmlSafe(`<code>${Ember.Handlebars.Utils.escapeExpression(errorJson)}</code>`) :
-    undefined;
-  return {
-    message,
-    errorJsonString: errorJson,
-  };
+
+  return findTranslation(
+    i18n,
+    i18nPrefix + errorIdToTranslate,
+    errorDetailsToTranslate
+  );
+}
+
+function findTokenTypeTranslation(i18n, tokenType) {
+  let translation = '';
+  if (tokenType.accessToken) {
+    translation =
+      findTranslation(i18n, i18nPrefix + 'translationParts.accessToken');
+  } else if (tokenType.inviteToken) {
+    translation =
+      tokenType.inviteToken.subtype + ' ' +
+      findTranslation(i18n, i18nPrefix + 'translationParts.inviteToken');
+  }
+  return translation;
+}
+
+function findTranslation(i18n, key, placeholders) {
+  const translation = i18n.t(key, placeholders);
+  const translationAsString = translation ? translation.toString() : '';
+  return (!translationAsString || translationAsString.startsWith('<missing-')) ?
+    undefined : translation;
+}
+
+function toPrintableString(string) {
+  return string && string.toString() ?
+    htmlSafe(Ember.Handlebars.Utils.escapeExpression(string)) : undefined;
+}
+
+function toPrintableJson(data) {
+  if (data === null || data === undefined || data === '') {
+    return undefined;
+  } else {
+    try {
+      const stringifiedJson = JSON.stringify(data, null, 2);
+      return htmlSafe(
+        `<code>${Ember.Handlebars.Utils.escapeExpression(stringifiedJson)}</code>`
+      );
+    } catch (e) {
+      return undefined;
+    }
+  }
 }
---
+++ tests/unit/utils/get-error-description-test.js
@@ -0,0 +1,349 @@
+import { expect } from 'chai';
+import { describe, it, beforeEach } from 'mocha';
+import getErrorDescription from 'onedata-gui-websocket-client/utils/get-error-description';
+import sinon from 'sinon';
+import { htmlSafe } from '@ember/string';
+import Ember from 'ember';
+
+describe('Unit | Utility | get error description', function () {
+  beforeEach(function () {
+    this.i18n = new I18nStub();
+  });
+
+  it('handles errors in form { id, details }', function () {
+    const testTranslation = 'translation';
+    sinon.stub(this.i18n, 't').returns(testTranslation);
+    const error = {
+      id: 'someError',
+      details: { a: 1 },
+    };
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: escapedHtmlSafe(testTranslation),
+      errorJsonString: escapedJsonHtmlSafe(error),
+    });
+  });
+
+  it('handles errors in form { id, details } when id is not recognized', function () {
+    sinon.stub(this.i18n, 't').returns('<missing-...');
+    const error = {
+      id: 'someError',
+      details: { a: 1 },
+    };
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: undefined,
+      errorJsonString: escapedJsonHtmlSafe(error),
+    });
+  });
+
+  it('handles errors in form { id, details } when JSON cannot be stringified', function () {
+    const testTranslation = 'translation';
+    sinon.stub(this.i18n, 't').returns(testTranslation);
+    const error = {
+      id: 'someError',
+      details: {},
+    };
+    // Circular structure which cannot be stringified
+    error.details = error;
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: escapedHtmlSafe(testTranslation),
+      errorJsonString: undefined,
+    });
+  });
+
+  it('handles errors in form { message }', function () {
+    const error = { message: 'someError' };
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: escapedHtmlSafe(error.message),
+      errorJsonString: undefined,
+    });
+  });
+
+  it('handles errors inside htmlSafe', function () {
+    const testTranslation = 'translation';
+    const error = htmlSafe(testTranslation);
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: escapedHtmlSafe(testTranslation),
+      errorJsonString: undefined,
+    });
+  });
+
+  it('handles non-standard object errors', function () {
+    const error = { err: 'error' };
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: undefined,
+      errorJsonString: escapedJsonHtmlSafe(error),
+    });
+  });
+
+  it('handles non-standard object errors with circular relations', function () {
+    const error = {};
+    error.err = error;
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: undefined,
+      errorJsonString: undefined,
+    });
+  });
+
+  it('handles errors in form { id, details } with id == "posix"', function () {
+    sinon.stub(this.i18n, 't')
+      .withArgs('errors.backendErrors.posix', { errno: 'enoent error' })
+      .returns('complete enoent error')
+      .withArgs('errors.backendErrors.translationParts.posixErrno.enoent')
+      .returns('enoent error');
+    const error = {
+      id: 'posix',
+      details: { errno: 'enoent' },
+    };
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: escapedHtmlSafe('complete enoent error'),
+      errorJsonString: escapedJsonHtmlSafe(error),
+    });
+  });
+
+  it('handles errors in form { id, details } with id == "posix" and unknown errno', function () {
+    const error = {
+      id: 'posix',
+      details: { errno: 'something' },
+    };
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: undefined,
+      errorJsonString: escapedJsonHtmlSafe(error),
+    });
+  });
+
+  [
+    'badAudienceToken',
+    'badValueToken',
+  ].forEach(errorId => {
+    it(`handles errors in form { id, details } with id == "${errorId}"`, function () {
+      const nestedErrorDetails = {
+        limit: 120,
+      };
+      const nestedError = {
+        id: 'tokenTooLarge',
+        details: nestedErrorDetails,
+      };
+      sinon.stub(this.i18n, 't')
+        .withArgs(
+          `errors.backendErrors.${errorId}`,
+          sinon.match({ tokenError: 'token too large' })
+        ).returns('complete error')
+        .withArgs(
+          'errors.backendErrors.tokenTooLarge',
+          nestedErrorDetails
+        ).returns('token too large');
+      const error = {
+        id: errorId,
+        details: {
+          tokenError: nestedError,
+        },
+      };
+  
+      const result = getErrorDescription(error, this.i18n);
+  
+      expect(result).to.deep.equal({
+        message: escapedHtmlSafe('complete error'),
+        errorJsonString: escapedJsonHtmlSafe(error),
+      });
+    });
+  });
+
+  it('handles errors in form { id, details } with id == "notAnAccessToken" and received with "accessToken"',
+    function () {
+      const tStub = sinon.stub(this.i18n, 't')
+        .withArgs(
+          'errors.backendErrors.notAnAccessToken',
+          sinon.match({ received: 'access token' })
+        ).returns('complete error');
+      stubAccessTokenTypeTranslation(tStub);
+      const error = {
+        id: 'notAnAccessToken',
+        details: {
+          received: {
+            accessToken: {},
+          },
+        },
+      };
+  
+      const result = getErrorDescription(error, this.i18n);
+  
+      expect(result).to.deep.equal({
+        message: escapedHtmlSafe('complete error'),
+        errorJsonString: escapedJsonHtmlSafe(error),
+      });
+    }
+  );
+
+  it('handles errors in form { id, details } with id == "notAnAccessToken" and received with "inviteToken"',
+    function () {
+      const tStub = sinon.stub(this.i18n, 't')
+        .withArgs(
+          'errors.backendErrors.notAnAccessToken',
+          sinon.match({ received: 'userToSpace invite token' })
+        ).returns('complete error');
+      stubInviteTokenTypeTranslation(tStub);
+      const error = {
+        id: 'notAnAccessToken',
+        details: {
+          received: {
+            inviteToken: {
+              subtype: 'userToSpace',
+            },
+          },
+        },
+      };
+  
+      const result = getErrorDescription(error, this.i18n);
+  
+      expect(result).to.deep.equal({
+        message: escapedHtmlSafe('complete error'),
+        errorJsonString: escapedJsonHtmlSafe(error),
+      });
+    }
+  );
+
+  it('handles errors in form { id, details } with id == "notAnInviteToken", expected with "accessToken" and received with "inviteToken"',
+    function () {
+      const tStub = sinon.stub(this.i18n, 't')
+        .withArgs(
+          'errors.backendErrors.notAnInviteToken',
+          sinon.match({
+            expected: 'access token',
+            received: 'userToSpace invite token',
+          })
+        ).returns('complete error');
+      stubAccessTokenTypeTranslation(tStub);
+      stubInviteTokenTypeTranslation(tStub);
+      const error = {
+        id: 'notAnInviteToken',
+        details: {
+          expected: {
+            accessToken: {},
+          },
+          received: {
+            inviteToken: {
+              subtype: 'userToSpace',
+            },
+          },
+        },
+      };
+  
+      const result = getErrorDescription(error, this.i18n);
+  
+      expect(result).to.deep.equal({
+        message: escapedHtmlSafe('complete error'),
+        errorJsonString: escapedJsonHtmlSafe(error),
+      });
+    }
+  );
+
+  it('handles errors in form { id, details } with id == "notAnInviteToken", expected with "inviteToken" and received with "accessToken"',
+    function () {
+      const tStub = sinon.stub(this.i18n, 't')
+        .withArgs(
+          'errors.backendErrors.notAnInviteToken',
+          sinon.match({
+            expected: 'userToSpace invite token',
+            received: 'access token',
+          })
+        ).returns('complete error');
+      stubAccessTokenTypeTranslation(tStub);
+      stubInviteTokenTypeTranslation(tStub);
+      const error = {
+        id: 'notAnInviteToken',
+        details: {
+          expected: {
+            inviteToken: {
+              subtype: 'userToSpace',
+            },
+          },
+          received: {
+            accessToken: {},
+          },
+        },
+      };
+  
+      const result = getErrorDescription(error, this.i18n);
+  
+      expect(result).to.deep.equal({
+        message: escapedHtmlSafe('complete error'),
+        errorJsonString: escapedJsonHtmlSafe(error),
+      });
+    }
+  );
+
+  it('handles errors in form { id, details } with id == "tokenAudienceForbidden"', function () {
+    sinon.stub(this.i18n, 't')
+      .withArgs('errors.backendErrors.tokenAudienceForbidden', {
+        audience: 'user:123',
+      }).returns('complete error');
+    const error = {
+      id: 'tokenAudienceForbidden',
+      details: {
+        audience: {
+          type: 'user',
+          id: '123',
+        },
+      },
+    };
+
+    const result = getErrorDescription(error, this.i18n);
+
+    expect(result).to.deep.equal({
+      message: escapedHtmlSafe('complete error'),
+      errorJsonString: escapedJsonHtmlSafe(error),
+    });
+  });
+});
+
+function stubInviteTokenTypeTranslation(tStub) {
+  return tStub
+    .withArgs('errors.backendErrors.translationParts.inviteToken')
+    .returns('invite token');
+}
+
+function stubAccessTokenTypeTranslation(tStub) {
+  return tStub
+    .withArgs('errors.backendErrors.translationParts.accessToken')
+    .returns('access token');
+}
+
+function escapedHtmlSafe(content) {
+  return htmlSafe(Ember.Handlebars.Utils.escapeExpression(content));
+}
+
+function escapedJsonHtmlSafe(content) {
+  const stringifiedJson = JSON.stringify(content, null, 2);
+  return htmlSafe(`<code>${Ember.Handlebars.Utils.escapeExpression(stringifiedJson)}</code>`);
+}
+
+class I18nStub {
+  t() {}
+}
