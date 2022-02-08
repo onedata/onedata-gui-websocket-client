28ca1ce6ffecf23c95790b46b2094e67b6b6a231
Michał Borzęcki <m.borzecki@cyfronet.pl>
2022-02-08 15:01:09 +0100

    VFS-8794 Added globalTimeSeconds handshake attribute

--- addon/services/onedata-connection.js
+++ addon/services/onedata-connection.js
@@ -1,12 +1,13 @@
 /**
- * An abstraction layer for Onedata Sync API Websocket connection properties 
+ * An abstraction layer for Onedata Sync API Websocket connection properties
  *
  * @module services/onedata-connection
  * @author Jakub Liput, Michał Borzęcki
- * @copyright (C) 2018-2019 ACK CYFRONET AGH
+ * @copyright (C) 2018-2022 ACK CYFRONET AGH
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
+import { observer } from '@ember/object';
 import { reads } from '@ember/object/computed';
 import Service, { inject as service } from '@ember/service';
 
@@ -14,17 +15,46 @@ export default Service.extend({
   onedataWebsocket: service(),
 
   /**
-   * @type {Ember.ComputedProperty<Object>}
+   * Difference between global (backend) time and local browser time.
+   * If > 0 then backend has "bigger" time.
+   * @type {ComputedProperty<number>}
+   */
+  globalTimeSecondsOffset: undefined,
+
+  /**
+   * @type {ComputedProperty<Object>}
    */
   attributes: reads('onedataWebsocket.connectionAttributes'),
 
   /**
-   * @type {Ember.ComputedProperty<string>}
+   * @type {ComputedProperty<string>}
    */
   serviceVersion: reads('attributes.serviceVersion'),
 
   /**
-   * @type {Ember.ComputedProperty<string>}
+   * @type {ComputedProperty<string>}
    */
   serviceBuildVersion: reads('attributes.serviceBuildVersion'),
+
+  /**
+   * Backend "now" timestamp during the WS handshake.
+   * @type {ComputedProperty<number>}
+   */
+  globalTimeSeconds: reads('attributes.globalTimeSeconds'),
+
+  globalTimeSecondsObserver: observer(
+    'globalTimeSeconds',
+    function globalTimeSecondsObserver() {
+      const nowTimestamp = Math.floor(Date.now() / 1000);
+      const globalTimeSeconds = this.get('globalTimeSeconds') || nowTimestamp;
+
+      this.set('globalTimeSecondsOffset', globalTimeSeconds - nowTimestamp);
+    }
+  ),
+
+  init() {
+    this._super(...arguments);
+
+    this.globalTimeSecondsObserver();
+  },
 });
