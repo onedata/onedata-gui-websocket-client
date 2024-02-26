959ee2de414a51e8f87222176140ceddb9076cbd
Jakub Liput <jakub.liput@gmail.com>
2024-02-26 11:00:15 +0100

    VFS-11710 Refactored isCrossOriginIframe util

--- addon/utils/is-cross-origin-iframe.js
+++ addon/utils/is-cross-origin-iframe.js
@@ -7,15 +7,13 @@
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
+import globals from 'onedata-gui-common/utils/globals';
+
 export default function isCrossOriginIframe() {
-  let parentOrigin;
   try {
-    parentOrigin = window.parent.location.origin;
+    return globals.window.location.origin !== globals.window.parent.location.origin;
   } catch {
     // getting parent origin means that it is blocked for security reasons
-  }
-  if (!parentOrigin) {
     return true;
   }
-  return window.location.origin !== parentOrigin;
 }
