90b3027017615269d201657b58f1d6896da6984f
Jakub Liput <jakub.liput@gmail.com>
2024-10-17 14:36:43 +0200

    VFS-12100 Adjusted tests to async observers

--- tests/integration/mixins/models/graph-single-model-test.js
+++ tests/integration/mixins/models/graph-single-model-test.js
@@ -9,6 +9,7 @@ import { lookupService } from '../../../helpers/stub-service';
 import useLocalStorageForStore from '../../../helpers/use-local-storage-for-store';
 import StaticGraphModelMixin from 'onedata-gui-websocket-client/mixins/models/static-graph-model';
 import resetStorages from 'ember-local-storage/test-support/reset-storage';
+import { settled } from '@ember/test-helpers';
 
 describe('Integration | Mixin | graph-single-model', function () {
   const { beforeEach, afterEach } = setupTest();
@@ -35,6 +36,8 @@ describe('Integration | Mixin | graph-single-model', function () {
 
       await record.save();
       await record.destroyRecord();
+      // recalculation is done by async observer
+      await settled();
 
       expect(recalculateSpy).to.have.been.calledOnce;
     }
