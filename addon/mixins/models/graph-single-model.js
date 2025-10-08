73af22011761ef0e4a4a4548a0888109ba0f22de
Jakub Liput <jakub.liput@gmail.com>
2025-10-08 16:42:36 +0200

    VFS-12896 Added forceInit flag to reload record list of record

--- addon/mixins/models/graph-single-model.js
+++ addon/mixins/models/graph-single-model.js
@@ -3,7 +3,7 @@
  *
  * @author Michał Borzęcki, Jakub Liput
  * @copyright (C) 2018-2024 ACK CYFRONET AGH
- * @copyright (C) 2025 Onedata
+ * @copyright (C) 2025 Onedata (onedata.org)
  * @license This software is released under the MIT license cited in 'LICENSE.txt'.
  */
 
@@ -24,6 +24,7 @@ import { DebouncedBatchFlushStrategy } from 'onedata-gui-websocket-client/utils/
  * @typedef {Object} ReloadRecordListOptions
  * @property {boolean} [reloadRecords] If true, reload method reloads each record from the
  *   list that has been already loaded into the store.
+ * @property {boolean} [forceInit] If true, load data if the list was not loaded earlier.
  */
 
 /**
@@ -31,6 +32,7 @@ import { DebouncedBatchFlushStrategy } from 'onedata-gui-websocket-client/utils/
  */
 const defaultReloadRecordListOptions = Object.freeze({
   reloadRecords: false,
+  forceInit: false,
 });
 
 export default Mixin.create(GraphModel, {
@@ -68,7 +70,7 @@ export default Mixin.create(GraphModel, {
   },
 
   /**
-   * Reloads list relation of record.If list has not been fetched, nothing is reloaded.
+   * Reloads list relation of record. If list has not been fetched, nothing is reloaded.
    * Optionally, you can enable `reloadRecords` which reloads each record from the list
    * that has been already loaded into the store.
    * @param {string} listName
@@ -77,41 +79,46 @@ export default Mixin.create(GraphModel, {
    */
   async reloadList(listName, options) {
     const { store, recordRegistry } = this;
-    const { reloadRecords } = { ...defaultReloadRecordListOptions, ...options };
-    const listRecord = this.belongsTo(listName).value();
-    if (listRecord) {
-      await listRecord.reload();
+    const { reloadRecords, forceInit } = {
+      ...defaultReloadRecordListOptions,
+      ...options,
+    };
+    let listRecord = this.belongsTo(listName).value();
+    if (forceInit || listRecord) {
+      if (listRecord) {
+        await listRecord.reload();
+      } else {
+        listRecord = await this[listName];
+      }
       if (!reloadRecords) {
         return listRecord;
       }
-      const hasManyReference = listRecord.hasMany('list');
-      const list = hasManyReference.value();
-      if (list) {
-        const itemsGris = hasManyReference.ids();
-        const containerSpec = new GrisBatchContainerSpec(
-          OwsGraphOperation.Get,
-          itemsGris
-        );
-        const container = await this.batchRequestRegistry.createContainer(
-          containerSpec,
-          DebouncedBatchFlushStrategy
-        );
-        try {
-          const recordsInStore = itemsGris
-            .map(gri => {
-              const modelName = recordRegistry.getModelName(gri);
-              return modelName ? store.peekRecord(modelName, gri) : null;
-            })
-            .filter(Boolean);
-          for (const record of recordsInStore) {
-            record.reload();
-          }
-          await container.flush();
-        } finally {
-          this.batchRequestRegistry.destroyContainer(container);
+      const itemsGris = listRecord.hasMany('list').ids();
+      const containerSpec = new GrisBatchContainerSpec(
+        OwsGraphOperation.Get,
+        itemsGris
+      );
+      const container = await this.batchRequestRegistry.createContainer(
+        containerSpec,
+        DebouncedBatchFlushStrategy
+      );
+      try {
+        const recordsInStore = itemsGris
+          .map(gri => {
+            const modelName = recordRegistry.getModelName(gri);
+            return modelName ? store.peekRecord(modelName, gri) : null;
+          })
+          .filter(Boolean);
+        for (const record of recordsInStore) {
+          record.reload();
         }
+        // force list fetching while batch containers are made
+        listRecord.list;
+        await container.flush();
+      } finally {
+        this.batchRequestRegistry.destroyContainer(container);
       }
-      return list ?? listRecord;
+      return (await listRecord.list) ?? listRecord;
     }
   },
 
